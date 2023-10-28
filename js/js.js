d5bbe62c4f5685ac0b0d0e351e71a745e6095fb5
Made <jayasedanamade@gmail.com>
2023-10-28 20:00:21 +0800

    update 999

---
+++ js/js.js
@@ -0,0 +1,12 @@
+function tombol(){
+    var X = 0
+var y = prompt("MASUKAN NOMINAL SALDO YANG INGIN DI TF")
+
+if (y - 10){
+    elert("minimal nominal lebih dari 10")
+}
+else{
+    elert("Nominal Sudah Masuk!")
+}
+
+}
